56dd71d1cde715f463de0376e18647766a72bc60
Jari Saari <jari.m.saari@gmail.com>
2025-04-06 20:20:44 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::collections::HashMap;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let vars: HashMap<String, String> = env::vars().collect();
+    let ip = match vars.get("ACTIX_IP") {
+        Some(p) => p.as_str(),
+        None => "127.0.0.1",
+    };
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind((ip, 8080))?
+    .run()
+    .await
+}
