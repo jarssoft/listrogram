1d3bd25a8689b1c6a0353c5a83dba3263c3ab67c
Jari Saari <jari.m.saari@gmail.com>
2025-04-10 01:17:06 +0300

    Tekstimuotoisen ohjelmatiedon jäsentäminen valmis

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,28 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+fn formatline(line: (usize, &str)) -> String {
+    format!("line {} \"{}\"", line.0+1, line.1)
+}
+
 #[post("/addtext")]
 async fn echo(req_body: String) -> impl Responder {
     
-    let mut progs: Vec::<(NaiveTime, String)> = Vec::new();
-    let mut lines = req_body.lines().enumerate();
+    let progs={
+        let mut progs: Vec::<(NaiveTime, String)> = Vec::new();
+        let mut lines = req_body.lines().enumerate();
 
-    while let Some(time) = lines.next() {    
-        let time_only = NaiveTime::parse_from_str(time.1, "%H:%M");
-        assert!(time_only.is_ok(), "Error in line {}. Expected time (%H:%M), found {}.", time.0+1, time.1);
+        while let Some(line1) = lines.next() {    
+            let time = NaiveTime::parse_from_str(line1.1, "%H:%M");
+            assert!(time.is_ok(), "Error in {}, expected time (%H:%M).", formatline(line1));
 
-        let title = lines.next();       
-        assert!(title.is_some(), "Expected program title, found end of file.");
-        assert!(title.unwrap().1.len()>0, "Error in line {}. Expected program title (a string longer than 0).", title.unwrap().0+1);
+            let line2 = lines.next().expect("Found end of file, expected program title.");       
+            assert!(line2.1.len()>0, "Error in {}, expected program title (a string longer than 0).", formatline(line2));
 
-        progs.push((time_only.unwrap(), title.unwrap().1.to_string().clone()));
-    }
-
+            progs.push((time.unwrap(), line2.1.to_string().clone()));
+        }
+        progs
+    };
     println!{"{progs:?}"}
     HttpResponse::Ok().body(req_body)
 
--- tyoajat.md
+++ tyoajat.md
@@ -4,8 +4,8 @@
 | :----:|:-----| :-----|
 | 25.3. | 5    | Projektin [hahmottelua](hahmotelma.md) |
 | 4.4.  | 5    | Virtuaalipalvelimen säätöä |
-| 6.4.  | 5    | Rustin kokeilua virtuaalipalvelimella |
-| 9.4.  | 3    | Tekstimuotoisen ohjelmatiedon syöttö, osa 1 |
+| 6.4.  | 5    | Rustin kokeilua virtuaalipalvelimella  |
+| 9.4.  | 5    | Tekstimuotoisen ohjelmatiedon jäsennys |
 
-Aikaa käytetty: 18 h
+Aikaa käytetty: 20 h
 Aikatavoie: 175 h
