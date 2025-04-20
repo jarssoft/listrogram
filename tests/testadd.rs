2f4d59561f8a8c3eb14fcb6ac3b4930cb2390086
Jari Saari <jari.m.saari@gmail.com>
2025-04-20 10:24:04 +0300

    Testaa response body, kun ohjelmatietoa lisätään tekstimuodossa. Test response body of adding progs as text

--- tests/testadd.rs
+++ tests/testadd.rs
@@ -3,6 +3,7 @@
 mod tests {
     use listagram::handlers::{add::addtext, *};
     use actix_web::{test, web, App};
+    use chrono::NaiveTime;
 
     #[actix_web::test]
     async fn test_addtext_post_without_payload() {
@@ -28,7 +29,7 @@ mod tests {
                 .service(addtext),
             ).await;
 
-        let payload ="99:20\nEfter Nio\n16:20\nElossa 24h\n16:50\nNovosti Yle";
+        let payload ="99:20\nEfter Nio\n16:20\nElossa 24h";
         let req = test::TestRequest::post().uri("/addtext").set_payload(payload).to_request();
 
         let resp = test::call_service(&app, req).await;
@@ -44,7 +45,7 @@ mod tests {
                 .service(addtext),
             ).await;
 
-        let payload ="15:20\nEfter Nio\n16:20\nElossa 24h\n16:50";
+        let payload ="15:20\nEfter Nio\n16:20";
         let req = test::TestRequest::post().uri("/addtext").set_payload(payload).to_request();
 
         let resp = test::call_service(&app, req).await;
@@ -60,7 +61,7 @@ mod tests {
                 .service(addtext),
             ).await;
 
-        let payload ="15:20\nEfter Nio\n15:00\nElossa 24h\n16:50";
+        let payload ="15:20\nEfter Nio\n15:00\nElossa 24h";
         let req = test::TestRequest::post().uri("/addtext").set_payload(payload).to_request();
 
         let resp = test::call_service(&app, req).await;
@@ -76,7 +77,7 @@ mod tests {
                 .service(addtext),
             ).await;
 
-        let payload ="15:20\n\n15:00\nElossa 24h\n16:50";
+        let payload ="15:20\n\n15:00\nElossa 24h";
         let req = test::TestRequest::post().uri("/addtext").set_payload(payload).to_request();
 
         let resp = test::call_service(&app, req).await;
@@ -92,28 +93,51 @@ mod tests {
                 .service(addtext),
             ).await;
 
-        let payload ="15:20\nEfter Nio\n16:20\nElossa 24h\n16:50\nNovosti Yle";
+        let payload ="15:20\nEfter Nio\n16:20\nElossa 24h\n";
         let req = test::TestRequest::post().uri("/addtext").set_payload(payload).to_request();
 
         let resp = test::call_service(&app, req).await;
         assert!(resp.status().is_success());
     }
 
+    #[actix_web::test] 
+    async fn test_addtext_response_is_exact() {
 
-            //let resp = test::call_service(& app, req).await;        
-        //let resp: AppState = test::call_and_read_body_json(&app, req).await;
-        
-        //print!("resp = '{resp:?}'");
-        ////let resp = test::call_and_read_body(&app, req).await;
-        ////print!("resp = '{resp:?}'");
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(build_appdata(None)))
+                .service(addtext),
+            ).await;
 
-        /*let body = resp.take_body();
-        let body = body.as_ref().unwrap();
-        
-        //call_and_read_body_json(&app, req).await;
-        assert!(resp.status().is_success());
+        let payload ="15:20\nEfter Nio\n16:20\nElossa 24h\n";
+        let req = test::TestRequest::post().uri("/addtext").set_payload(payload).to_request();
+
+        /*
+        let resp = test::call_service(&app, req).await;        
+        let bytes = actix_web::body::to_bytes(resp.into_body()).await.unwrap();
         */
 
+        let bytes = test::call_and_read_body(&app, req).await;        
+        let str = std::str::from_utf8(&bytes).unwrap();
+        assert!(str.eq("[[\"15:20:00\",\"Efter Nio\"],[\"16:20:00\",\"Elossa 24h\"]]"));
+        print!("resp = '{}'", str);
+    }
 
+    #[actix_web::test] 
+    async fn test_addtext_response_json_is_correct() {
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(build_appdata(None)))
+                .service(addtext),
+            ).await;
+
+        let payload ="15:20\nEfter Nio\n16:20\nElossa 24h\n";
+        let req = test::TestRequest::post().uri("/addtext").set_payload(payload).to_request();
+ 
+        let resp: Vec<(NaiveTime, String)> = test::call_and_read_body_json(&app, req).await;
+        print!("resp = '{:?}'", resp);
+        assert!(resp.len()==2);
+    }
 
 }
